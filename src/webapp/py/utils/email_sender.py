7047e3fca3a3c7f527ed76acdcfc2fbedfe6d78d
Marios Chartsias <mariosxama@gmail.com>
2024-03-11 20:45:00 +0200

    init class of email sender

--- src/webapp/py/utils/email_sender.py
+++ src/webapp/py/utils/email_sender.py
@@ -1,41 +1,38 @@
 import smtplib
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from random import randint
 
 class EmailSender:
-    def __init__(self):
-        self.smtp_server = 'smtpout.europe.secureserver.net' #'smtp-mail.outlook.com'
-        self.smtp_port = 465 #587
-        self.sender_email = 'info@playbotworld.com'
-        self.sender_password = 'rT3$y9p2#x!'
-
-    def send_email(self, recipient_email, verif_code):
-        # Create a message object
+    def __init__(self, sender_email, sender_password):
+        self.sender_email = sender_email
+        self.sender_password = sender_password
+        self.smtp_server = 'smtpout.secureserver.net'
+        self.smtp_port = 465  # 465 is the default port for SMTP over SSL/TLS
+    
+    def send_email(self, receiver_email, subject, body):
+        # Create a MIME multipart message
         message = MIMEMultipart()
         message['From'] = self.sender_email
-        message['To'] = recipient_email
-        message['Subject'] = 'Please confirm your email address'
+        message['To'] = receiver_email
+        message['Subject'] = subject
 
-        # Add the email body
-        body = verif_code
+        # Attach the body to the email
         message.attach(MIMEText(body, 'plain'))
 
-        # Connect to the SMTP server
-        with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
-            server.starttls()
-            server.login(self.sender_email, self.sender_password)
+        # Create SMTP session
+        server = smtplib.SMTP_SSL(self.smtp_server, self.smtp_port)  # SMTP_SSL for SSL/TLS
+        server.login(self.sender_email, self.sender_password)  # Login
 
-            # Send the email
-            server.send_message(message)
+        # Send the email
+        server.sendmail(self.sender_email, receiver_email, message.as_string())
 
-            # Send email
-            server.sendmail(self.sender_email, recipient_email, message.as_string())
-        
-        print("Email sent successfully!")
+        # Quit the SMTP session
+        server.quit()
+
 
 
 if __name__ == '__main__':
-    email_sender = EmailSender()
-    email_sender.send_email('mytoplux@gmail.com','geia')
-
-
+    # Example usage:
+    sender = EmailSender('info@playbotworld.com', 'rT3$y9p2#x') #email credentials
+    sender.send_email('mytoplux@gmail.com', 'Please activate your account', f'Your temporary code is {randint(1000,9999)} ')
